aca8d2e582cb4b9c87c7def82ee52fdad2aeaebd
CyraIftikhar <112282875+CyraIftikhar@users.noreply.github.com>
2022-11-09 23:46:04 +0000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        <a href="https://github.com/CyraIftikhar/react-weather-app">
+          Open-source
+        </a>{" "}
+        coded by Cyra Iftikhar
+      </footer>
+    </div>
+  );
 }
