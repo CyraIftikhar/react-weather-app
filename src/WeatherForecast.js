852cbc64283ae0118390903499ca3ca7f6925d66
CyraIftikhar <112282875+CyraIftikhar@users.noreply.github.com>
2022-11-13 21:01:57 +0000

    created loop for forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -3,37 +3,42 @@ import "./WeatherForecast.css";
 import axios from "axios";
 
 export default function WeatherForecast(props) {
-    let [loaded, setLoaded] = useState(false);
-    let [forecast, setForecast] = useState(null);
-
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
 
   function handleResponse(response) {
-setForecast(response.data.daily)
-setLoaded(true);
+    setForecast(response.data.daily);
+    setLoaded(true);
   }
 
   if (loaded) {
-      return (
-  <div classname="WeatherForecast">
-      <div className="row">
-        <div className="col">
-            <WeatherForecastDay data={forecast[0]}/>
+    return (
+      <div classname="WeatherForecast">
+        <div className="row">
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
+          </div>
         </div>
       </div>
-    </div>
-      );
+    );
   } else {
-return (
-let apiKey = "08o0558e3e9fc6bt6ff6f508603a7424";
-  let longitude = props.coordinates.lon;
-  let latitude = props.coordinates.lat;
+    let apiKey = "08o0558e3e9fc6bt6ff6f508603a7424";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
 
-  let apiURL = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
+    let apiURL = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
 
-  axios.get(apiURL).then(handleResponse);
-  
-  return null;
+    axios.get(apiURL).then(handleResponse);
 
-  );
-}
+    return null;
+  }
 }
